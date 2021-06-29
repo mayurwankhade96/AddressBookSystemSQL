63f29eb4b8afe01527bfe6f339f227e13df63f8c
Mayur Wankhade <mayur.wankhade2@gmail.com>
2021-06-30 01:22:04 +0530

    [Mayur] Refactor : Created address_book table.

--- address_book_service.sql
+++ address_book_service.sql
@@ -1,3 +1,15 @@
 CREATE DATABASE address_book_service;
 
 USE address_book_service;
+
+CREATE TABLE address_book
+(
+	first_name VARCHAR(20) NOT NULL,
+	last_name VARCHAR(20) NOT NULL,
+	address VARCHAR(255) NOT NULL,
+	city VARCHAR(20) NOT NULL,
+	state VARCHAR(20) NOT NULL,
+	zip INT NOT NULL,
+	phone_number BIGINT NOT NULL,
+	email VARCHAR(255) NOT NULL
+);
